38878c8252fe8e61d37f619d001cdf289b1430b1
KasperTell <kaspertell@gmail.com>
2024-02-07 10:17:54 +0100

    Kopi af template kode

--- Api/Program.cs
+++ Api/Program.cs
@@ -1,44 +1,40 @@
+using System.Reflection;
+using Fleck;
+using lib;
+using ws;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+var clientEventHandlers = builder.FindAndInjectClientEventHandlers(Assembly.GetExecutingAssembly());
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+var server = new WebSocketServer("ws://0.0.0.0:8181");
+
+
+server.Start(ws =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    ws.OnOpen = () =>
+    {
+        StateService.AddConnection(ws);
+    };
+    ws.OnMessage = async message =>
+    {
+        // evaluate whether or not message.eventType == 
+        // trigger event handler
+        try
+        {
+            await app.InvokeClientEventHandler(clientEventHandlers, ws, message);
 
-app.UseHttpsRedirection();
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            Console.WriteLine(e.InnerException);
+            Console.WriteLine(e.StackTrace);
+            // your exception handling here
+        }
+    };
+});
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
-.WithOpenApi();
-
-app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+Console.ReadLine();
