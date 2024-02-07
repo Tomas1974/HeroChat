5247da129d217a9d7c31076f831865787a45ce53
KasperTell <kaspertell@gmail.com>
2024-02-07 11:20:35 +0100

    SERVICE LAG

---
+++ Service/MessageService.cs
@@ -0,0 +1,16 @@
+﻿using infrastructure;
+
+namespace Service;
+
+public class MessageService(ChatMessageService _chatMessageRepository)
+{
+    public IEnumerable<MessageModel> GetMessages()
+    {
+        return _chatMessageRepository.GetMessages();
+    }
+    
+    public MessageModel CreateAvatar(MessageModel message)
+    {
+        return _chatMessageRepository.CreateMessage(message);
+    }
+}
