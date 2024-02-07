156574217f86ce1ff2c5d7fc55a108c8b00ba78e
KasperTell <kaspertell@gmail.com>
2024-02-07 11:04:29 +0100

    Infrastructure model and methods

--- infrastructure/ChatMessageService.cs
+++ infrastructure/ChatMessageService.cs
@@ -1,8 +1,28 @@
-﻿namespace infrastructure;
+﻿using Dapper;
+using Npgsql;
 
-public class ChatMessageService
+namespace infrastructure;
+
+public class ChatMessageService(NpgsqlDataSource _dataSource)
 {
+    public IEnumerable<MessageModel> GetMessages ()
+    {
+        var sql = @"SELECT * FROM webshop.avatar where deleted=true ORDER BY avatar_id;";
+
+        using (var conn = _dataSource.OpenConnection())
+        {
+            return conn.Query<MessageModel>(sql);
+        }
+    }
     
-    
-    
+    public MessageModel CreateAvatar(MessageModel message)
+    {
+        var sql =
+            @" INSERT INTO messages (ChatFrom,Room,ChatMessage) VALUES (@chatFrom, @room, @chatMessage) RETURNING *;";
+
+        using (var conn = _dataSource.OpenConnection())
+        {
+            return conn.QueryFirst<MessageModel>(sql, new { chatFrom=message.ChatFrom, room=message.Room, chatMessage=message.ChatMessage});
+        }
+    }
 }
