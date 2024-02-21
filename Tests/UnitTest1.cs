5869295eb0e779c009c1f1eff947ca9faad6a411
Tomas1974 <114471994+Tomas1974@users.noreply.github.com>
2024-02-21 10:16:13 +0100

    tests

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,10 +1,16 @@
+using Dapper;
 using lib;
+using Npgsql;
 using Websocket.Client;
+using ws;
 
 namespace Tests;
 
 public class Tests
 {
+    private static readonly Uri Uri = new(Environment.GetEnvironmentVariable("pgconn")!);
+    
+
     [SetUp]
     public void Setup()
     {
@@ -12,9 +18,57 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public async Task Test1()
     {
-        var wsClient= new WebsocketClient(new Uri("ws://localhost:8181"));
-        
+        var ws = await new WebSocketTestClient().ConnectAsync();
+        var ws2 = await new WebSocketTestClient().ConnectAsync();
+        await ws.DoAndAssert(new ClientWantsToSignInDto()
+        {
+            Username = "Bob"
+        }, r => r.Count(dto => dto.eventType == nameof(ServerWelcomesUser)) == 1);
+        await ws2.DoAndAssert(new ClientWantsToSignInDto()
+        {
+            Username = "Alice"
+        },r => r.Count(dto => dto.eventType == nameof(ServerWelcomesUser)) == 1);
+
+
+        await ws.DoAndAssert(new ClientWantsToEnterRoomDto()
+        {
+            roomId = 1
+        },r => r.Count(dto => dto.eventType == nameof(ServerWantToEnterRoom)) == 1);
+        await ws2.DoAndAssert(new ClientWantsToEnterRoomDto()
+        {
+            roomId = 1
+        },r => r.Count(dto => dto.eventType == nameof(ServerWantToEnterRoom)) == 1);
+
+         await ws.DoAndAssert(new ClientWantsToBroadcastToRoomDto()
+         {
+            message = "ABCDEFGHIJKLMNOPQRSTUVXYZÆØÅ",
+            roomId = 1
+            
+         },r => r.Count(dto => dto.eventType == nameof(newMessageToStore)) == 1);
+         await ws2.DoAndAssert(new ClientWantsToBroadcastToRoomDto()
+         {
+             roomId = 1,
+             message = "ABCDEFGHIJKLMNOPQRSTUVXYZÆØÅ"
+         },r => r.Count(dto => dto.eventType == nameof(newMessageToStore)) == 2);
+         
+         
+         
+         await ws.DoAndAssert(new ClientWantsToGetStoredMessagesToRoomDto()
+         {
+             roomId = 1
+         },r => r.Count(dto => dto.eventType == nameof(ServerSendStoredMessageToClient)) == 1);
+      
+         
+         await ws2.DoAndAssert(new ClientWantsToGetStoredMessagesToRoomDto()
+         {
+             roomId = 1
+         },r => r.Count(dto => dto.eventType == nameof(ServerSendStoredMessageToClient)) == 1);
+
+         using (var conn = Helper.DataSource.OpenConnection())
+         {
+             conn.Execute("DELETE FROM messages WHERE chatmessage = 'ABCDEFGHIJKLMNOPQRSTUVXYZÆØÅ'");
+         }
     }
 }
